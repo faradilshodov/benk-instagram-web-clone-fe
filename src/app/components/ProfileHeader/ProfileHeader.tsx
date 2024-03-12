9501eb7e3ef3b3bd120b925b5cc3ed7de2ff42a8
faradilshodov <farrukh.dilshodov@gmail.com>
2024-03-12 10:04:43 -0400

    Add starter code for Header

--- src/app/components/ProfileHeader/ProfileHeader.tsx
+++ src/app/components/ProfileHeader/ProfileHeader.tsx
@@ -1,7 +1,35 @@
-export default function ProfileHeader() {
+import { useEffect } from "react";
 
-    
+const apiLink = "https://jan24-jilhslxp5q-uc.a.run.app/api/user";
+
+export default function ProfileHeader() {
+    // start displaying data with useState
+
+    // fetch some data using  useEffect
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response: Response = await fetch("https://jan24-jilhslxp5q-uc.a.run.app/api/user");
+
+                if (!response.ok) {
+                    throw new Error ("Network response not ok!");
+                };
+
+                const responseJSON = await response.json();
+
+                console.log(responseJSON);
+                
+
+            } catch (error: any) {
+                console.log(error);
+                
+            };
+        };
+
+        fetchData();
+    }, [])
+
     return (
         <p>Profile Header</p>
-    )
-}
+    );
+};
